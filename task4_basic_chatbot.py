2f83dc8b0dccf7137ade6f34ea0c057d91cb8642
Malla Sai Vardan <mallasaivardan@gmail.com>
2026-06-29 16:09:14 +0530

    Add files via upload

---
+++ task4_basic_chatbot.py
@@ -0,0 +1,17 @@
+def chatbot():
+    print("Chatbot: Hi! Type bye to exit.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Chatbot: Hi!")
+        elif user == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+        elif user == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        else:
+            print("Chatbot: I don't understand.")
+
+chatbot()
